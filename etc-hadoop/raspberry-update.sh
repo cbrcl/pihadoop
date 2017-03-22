6381299e06441364a38b3f6de46b6840449dec2f
cbrcl <cristian@khn.cl>
2017-03-22 11:52:43 +0100

    Update raspberry-update.sh

--- etc-hadoop/raspberry-update.sh
+++ etc-hadoop/raspberry-update.sh
@@ -3,6 +3,3 @@ sudo apt-get upgrade
 sudo apt-get install python3-pip
 sudo apt-get install zip unzip
 sudo apt-get install oracle-java7-jdk
-sudo apt-get install git 
-git clone https://github.com/cbrcl/pihadoop
-chmod +x pihadoop/etc-hadoop/raspberry-hadoop-folders.sh

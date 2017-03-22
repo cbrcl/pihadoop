0b8998d3039980fab6c52576aca9891f7eb989d2
cbrcl <cristian@khn.cl>
2017-03-22 11:26:44 +0100

    Update raspberry-cluster-hadoop.sh

--- etc-hadoop/raspberry-cluster-hadoop.sh
+++ etc-hadoop/raspberry-cluster-hadoop.sh
@@ -1,11 +1,3 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install python3-pip
-sudo apt-get install zip unzip
-sudo apt-get install oracle-java7-jdk
-
-
-
 sudo addgroup hadoop
 sudo adduser --ingroup hadoop hduser
 sudo adduser hduser sudo

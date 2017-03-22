1f0ffade1f583dc2bf3ba2ab53685a6165ef2f1f
cbrcl <cristian@khn.cl>
2017-03-22 11:30:47 +0100

    split config steps

--- etc-hadoop/raspberry-cluster-hadoop.sh
+++ etc-hadoop/raspberry-cluster-hadoop.sh
@@ -2,5 +2,3 @@ sudo addgroup hadoop
 sudo adduser --ingroup hadoop hduser
 sudo adduser hduser sudo
 sudo hduser
-sudo mkdir /hdfs/tmp
-sudo chown -R hduser:hadoop /hdfs/tmp
---
+++ etc-hadoop/raspberry-hadoop-folders.sh
@@ -0,0 +1,2 @@
+sudo mkdir /hdfs/tmp
+sudo chown -R hduser:hadoop /hdfs/tmp
